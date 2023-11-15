5106e746147f495af5a54e78eb6109a823142cd5
Amirpooya <amiasa@student.chalmers.se>
2023-11-15 13:58:43 +0100

    #2 Patient end points
    Endpoints for patients have been moved to their own repo this gets rid of the codes related to the patients.

--- api-gateway/src/endpoints/v1/routesv1.ts
+++ api-gateway/src/endpoints/v1/routesv1.ts
@@ -1,10 +1,7 @@
 import { Express } from "express";
-import dentistRoutes from "./dentistRoutes"
-import patientRoutes from "./patientRoutes"
-
+import dentistRoutes from "./dentistRoutes";
 
 // All api routes will be added here 
 export function routerV1(app: Express) {
     app.use('/api/v1/dentists', dentistRoutes);
-    app.use('/api/v1/patients', patientRoutes);
 }
