24fc73c8616cae467c28a7f7b9cd7d584b81559a
Amirpooya <amiasa@student.chalmers.se>
2023-11-18 12:39:38 +0100

    #4 Current-user endpoint
    This adds an endpoint for receiving a user which is not introduced in the client side yet but we have a token for it

--- api-gateway/src/endpoints/v1/routesv1.ts
+++ api-gateway/src/endpoints/v1/routesv1.ts
@@ -1,7 +1,9 @@
 import { Express } from "express";
 import dentistRoutes from "./dentistRoutes";
+import dentistCurrent from './dentistCurrent'
 
 // All api routes will be added here 
 export function routerV1(app: Express) {
+    app.use('/api/v1/dentists/current-user', dentistCurrent)
     app.use('/api/v1/dentists', dentistRoutes);
 }
